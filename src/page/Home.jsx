90a3d6334d438f16b2b470a81adee191241ff060
Pukkok <krystalgggg@gmail.com>
2025-04-04 16:11:27 +0900

    [Feat] 검색창, 카카오맵 연결

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,8 +1,15 @@
+import SearchBar from "../components/SearchBar";
+import KakaoMap from "../components/KakaoMap";
+
 export default function Home() {
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
+    <div className="min-h-scree flex flex-col items-center p-6">
       <h1>홈화면</h1>
+      <SearchBar placeholder="검색어 입력" autoFocus={true}/>
+      <div className="fixed top-0 left-0 right-0 bottom-0 z-[-1]">
+        <KakaoMap />
+      </div>
     </div>
-  );
+  )
 }
