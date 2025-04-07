c234737471226375589450719b9817cb2cafe322
Pukkok <krystalgggg@gmail.com>
2025-04-07 21:42:36 +0900

    [Refact]홈 프로젝트 새로 시작

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,15 +1,7 @@
-import { useState } from "react"
-import KakaoMap from "../components/KakaoMap"
-import SideBar from "../components/SideBar/SideBar"
-
 const Home = () => {
-  const [addr, setAddr] = useState("세종특별자치시 나리로 38")
-  const [places, setPlaces] = useState([]) // 검색 결과 저장
-
   return (
-    <div className="min-h-screen flex">
-      <SideBar setAddr={setAddr} places={places} />
-      <KakaoMap addr={addr} setPlaces={setPlaces} />
+    <div className="min-h-screen p-4 bg-[#fff8f4]">
+  
     </div>
   )
 }
