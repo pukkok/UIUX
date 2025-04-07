0f88bcdcbdf2b5080a9df126a0f0d64a4b9f347e
Pukkok <krystalgggg@gmail.com>
2025-04-08 01:03:31 +0900

    [Feat] 맵 화면 구현 시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
+import FoodFinder from "./page/FoodFinder";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/map" element={<FoodFinder />}></Route>
     </Routes>
   )
 }
---
+++ src/page/FoodFinder.jsx
@@ -0,0 +1,11 @@
+const FoodFinder = () => {
+  
+  return (
+    <div>
+      카카오맵 가져오기
+    </div>
+  )
+
+}
+
+export default FoodFinder
