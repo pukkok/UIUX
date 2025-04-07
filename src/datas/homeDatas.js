222f5f27c1448c6ce9734b41dd3851b955952ece
Pukkok <krystalgggg@gmail.com>
2025-04-07 23:42:58 +0900

    [Feat] 홈 데이터 구성 파일 생성

---
+++ src/datas/homeDatas.js
@@ -0,0 +1,20 @@
+const homeDatas = [
+  {
+    point : "갈거야",
+    icon : "🚩"
+  },
+  {
+    point : "이거야",
+    icon : "🟢"
+  },
+  {
+    point : "고민이야",
+    icon : "🟡"
+  },
+  {
+    point : "아니야",
+    icon : "🔴"
+  }
+]
+
+export default homeDatas
---
+++ src/datas/woopaImgs.js
@@ -0,0 +1,8 @@
+import failImg from '../assets/fail.png';
+import readyTogoImg from '../assets/ready_to_go.png';
+import successImg from '../assets/success.png';
+import thinkAboutImg from '../assets/think_about.png';
+
+const woopaImgs = [readyTogoImg, successImg, thinkAboutImg, failImg]
+
+export default woopaImgs
