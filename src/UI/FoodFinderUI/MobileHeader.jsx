fa5124ba829f763bda464a88652623d870737f8e
Pukkok <krystalgggg@gmail.com>
2025-04-08 11:53:56 +0900

    [Feat] 앱으로 사용할 검색 헤더 제작

---
+++ src/UI/FoodFinderUI/MobileHeader.jsx
@@ -0,0 +1,28 @@
+import { useRef } from "react"
+import SearchBar from "./SearchBar"
+
+const MobileHeader = ({setAddr}) => {
+  const searchRef = useRef(null)
+
+  const search = () => {
+    if (searchRef.current?.value.trim()) {
+      setAddr(searchRef.current.value)
+    }
+  }
+
+  return (
+    <header className="
+    fixed top-0 left-0 z-10 
+    w-full h-16 
+    bg-transparent 
+    flex gap-4 items-center justify-between px-4">
+      <SearchBar 
+      ref={searchRef}
+      placeholder={"장소·주소 검색"}
+      search={search}
+      />
+    </header>
+  )
+}
+
+export default MobileHeader
